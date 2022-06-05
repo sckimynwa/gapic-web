e092e908b2e49f143ccd115c5031417545b01800
sckimynwa <tsb03672@naver.com>
2022-06-05 18:47:56 +0900

    feat: add dependency & make test

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,6 +1,14 @@
-import { Suspense } from "react";
+import { UserServiceClient } from 'questionspoke';
+import { Suspense, useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const userClient = new UserServiceClient({
+      fallback: 'rest',
+      protocol: 'http',
+    });
+    userClient.getUser({ name: 'me' }).then((data) => console.log(data));
+  }, []);
   return (
     <Suspense fallback={<></>}>
       <div>Hello World</div>
